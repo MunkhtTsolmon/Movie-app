e0eb602d89394692a360a16e0ba7a1c9aea58b4d
MunkhtTsolmon <monkhtsolmonmuzo@gmail.com>
2024-12-24 20:12:47 +0800

    first commit

---
+++ src/app/navigation1.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+export const Navigation = () => {
+  return (
+    <div className="mt-[0.5rem] p-[2rem] flex justify-between">
+      <div className="w-[5.75rem] flex gap-[0.3rem] text-indigo-700 items-center">
+        <div className="w-[1.25rem] h-[1.25rem]">
+          <svg
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4.83366 0.666748V17.3334M13.167 0.666748V17.3334M0.666992 9.00008H17.3337M0.666992 4.83341H4.83366M0.666992 13.1667H4.83366M13.167 13.1667H17.3337M13.167 4.83341H17.3337M2.48366 0.666748H15.517C16.5203 0.666748 17.3337 1.4801 17.3337 2.48341V15.5167C17.3337 16.5201 16.5203 17.3334 15.517 17.3334H2.48366C1.48034 17.3334 0.666992 16.5201 0.666992 15.5167V2.48341C0.666992 1.4801 1.48034 0.666748 2.48366 0.666748Z"
+              stroke="#4338CA"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
+        <p className="font-bold text-[1.1rem] italic mt-[-0.2rem]">Movie Z</p>
+      </div>
+      <div className="flex gap-[0.4rem]">
+        <button>
+          <Image src={"/search.png"} width={36} height={36} alt="search" />
+        </button>
+        <Image src={"/darkButton.png"} width={36} height={36} alt="dark" />
+      </div>
+    </div>
+  );
+};
---
+++ src/app/navigation2.tsx
@@ -0,0 +1,11 @@
+import { Input } from "@/components/ui/input";
+import { Select } from "@/components/ui/select";
+export const Navigation2 = () => {
+  return (
+    <div className="mt-[0.5rem] p-[2rem] flex justify-between">
+      <Select />
+      <Input />
+      <button>x</button>
+    </div>
+  );
+};
